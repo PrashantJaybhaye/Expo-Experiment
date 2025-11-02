3edf010920a619e68d521adc7c6258f0def9fb08
Prashant <prashan1837d@gmail.com>
2025-11-02 14:32:52 +0530

    feat: Implement Splash Screen and Layout Components with Theming

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router'
+import React from 'react'
+import { StyleSheet } from 'react-native'
 
-export default function RootLayout() {
-  return <Stack />;
+const _layout = () => {
+    return (
+        <Stack screenOptions={{ headerShown: false }} />
+    )
 }
+
+export default _layout
+
+const styles = StyleSheet.create({
+
+})
